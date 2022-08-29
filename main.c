91c826b289149656ecedc7fc8d6fa78ce3414d0e
Juan Pablo <pablo.lopez@iteso.mx>
2022-08-29 10:37:52 -0500

    version final

--- main.c
+++ main.c
@@ -2,11 +2,21 @@
 
 int main(void ) {
     int mi_matriz[5][5];
+    int *mi_super_pointer[5];
+
+    mi_super_pointer[0] = mi_matriz[0];
+    mi_super_pointer[1] = mi_matriz[1];
 
     mi_matriz[0][0] = 12;
+    mi_matriz[0][1] = 45;
+    mi_matriz[1][3] = 75;
+    mi_matriz[1][0] = 123;
 
-    int *apuntador_a_entero = mi_matriz[0][0];
+//    int *mi_arreglo = mi_matriz[0];
+//    int *mi_arreglo_dos = mi_matriz[1];
 
-    printf("%d", mi_matriz[0][0]);
+//    printf("%d", *(mi_arreglo_dos+3));
+    printf("%d\n", *(*(mi_super_pointer+1)+3));
+    printf("%d",*mi_matriz[0]);
     return 0;
 }
